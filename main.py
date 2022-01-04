ad400d86b8e614aa11460755dc24a15121f41a88
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-04 06:43:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Write a Python function to check whether a number is in a given range. Import the module to input a number and check whether is in a given range 
+import range1
+
+x=int(input())
+y=int(input())
+m=int(input())
+print(range1.rangew(x,y,m))
+
---
+++ range1.py
@@ -0,0 +1,15 @@
+def rangew(n,p,z):
+  int(n)
+  int(p)
+  int(z)
+  i=n
+  int(i)
+  while (i<p):
+    if i==z:
+      c=1
+  
+    i=i+1
+  if(c!=1):
+    return"not fount"
+  else:
+    return"Fount"
